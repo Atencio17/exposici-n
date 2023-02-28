704f8b3aeeceb9ee49572016d1eb09f52719ded3
Atencio17 <atenciomuniveandresfelipe2@gmail.com>
2023-02-28 15:28:22 -0500

    primer commit

---
+++ layouts/layouts/layouts/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace layouts
+{
+    public partial class App : Application
+    {
+        public static FlyoutPage flyoutPage { get; set; }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ layouts/layouts/layouts/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace layouts
+{
+    public partial class MainPage : FlyoutPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Flyout = new maestro();
+            this.Detail = new NavigationPage(new detalle());
+
+            App.flyoutPage = this;
+        }
+    }
+}
